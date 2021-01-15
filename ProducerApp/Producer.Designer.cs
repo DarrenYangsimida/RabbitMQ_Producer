44547c5def5a1e2f93d9e39ea8cc7a2072daf74d
gdcdevcontractor6-c <gdcdevcontractor6-c@deloitte.com.cn>
2021-01-15 18:14:09 +0800

    新增 Producer 基本逻辑代码

---
+++ ProducerApp/Producer.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace ProducerApp
+{
+    partial class Producer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(517, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开启 MQ 服务";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(490, 219);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(517, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "发送消息";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(517, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "关闭 MQ 服务";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Producer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 250);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Producer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "生产者客户端";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+        /// <summary>
+        /// 开启 MQ 服务
+        /// </summary>
+        private System.Windows.Forms.Button button1;
+
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        private System.Windows.Forms.Button button2;
+
+        /// <summary>
+        /// 关闭 MQ 服务
+        /// </summary>
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ProducerApp/Producer.cs
@@ -0,0 +1,107 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProducerApp
+{
+    public partial class Producer : Form
+    {
+        private ConnectionFactory factory;
+        private IConnection connection;
+        private IModel channel;
+        private IBasicProperties properties;
+
+        public Producer()
+        {
+            InitializeComponent();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                factory = new ConnectionFactory()
+                {
+                    HostName = "localhost",//本地 rabbitmq 服务
+                    UserName = "guest",
+                    Password = "guest"
+                };
+                connection = factory.CreateConnection();
+                channel = connection.CreateModel();
+                channel.ExchangeDeclare(exchange: "SourceExchange", type: ExchangeType.Topic, durable: true, autoDelete: true, arguments: null);
+                channel.QueueDeclare(queue: "testQueue", durable: true, exclusive: false, autoDelete: true, arguments: null);
+                channel.QueueBind(queue: "testQueue", exchange: "SourceExchange", routingKey: "*.msg", arguments: null);
+                properties = channel.CreateBasicProperties();
+                properties.DeliveryMode = 2;
+
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"开启 MQ 服务失败，请重试！Error Message：{ex.Message}");
+                CloseMQConnection();
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CloseMQConnection();
+        }
+
+        public void CloseMQConnection()
+        {
+            try
+            {
+                channel.ExchangeDelete("SourceExchange");
+                channel.QueueDelete("testQueue");
+                channel.Close();
+                connection.Close();
+                connection.Dispose();
+            }
+            catch (Exception ex)
+            {
+                _ = ex.Message;
+            }
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string msg = richTextBox1.Text;
+                if (!string.IsNullOrWhiteSpace(msg))
+                {
+                    var body = Encoding.UTF8.GetBytes(msg);
+                    channel.BasicPublish(exchange: "SourceExchange", routingKey: $"{Guid.NewGuid()}.msg", basicProperties: properties, body: body);
+                    MessageBox.Show("消息发送成功");
+                    richTextBox1.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("请输入要发送的内容");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"消息发送失败！Error Message: {ex.Message}");
+            }
+        }
+
+    }
+}
